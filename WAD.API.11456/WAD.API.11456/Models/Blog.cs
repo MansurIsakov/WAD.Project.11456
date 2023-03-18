0886e47504923d749694f2f256424b6c0c9e7a15
MansurIsakov <mansurisakov.dev@gmail.com>
2023-03-18 16:35:05 +0500

    init: init backend

---
+++ WAD.API.11456/WAD.API.11456/Models/Blog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD.API._11456.Models
+{
+    public class Blog
+    {
+        public int id { get; set; }
+    }
+}
