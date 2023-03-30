35929e05e6329e49d0cb85fd813d153e5c070117
Mansur Isakov <72330622+MansurIsakov@users.noreply.github.com>
2023-03-30 12:02:52 +0500

    Delete WAD.Client.11456 directory

--- WAD.Client.11456/src/app/features/blog/components/blog-filtering/blog-filtering.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-blog-filtering',
-  templateUrl: './blog-filtering.component.html',
-  styleUrls: ['./blog-filtering.component.scss'],
-})
-export class BlogFilteringComponent {}
